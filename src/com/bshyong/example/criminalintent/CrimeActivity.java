e9d1c5bd003fadfcf57b990b1ae8c9177c85eeb6
Benjamin Shyong <ben.shyong@gmail.com>
2014-01-10 02:57:40 -0500

    call crimefragment from crimeactivity

--- src/com/bshyong/example/criminalintent/CrimeActivity.java
+++ src/com/bshyong/example/criminalintent/CrimeActivity.java
@@ -1,14 +1,27 @@
 package com.bshyong.example.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 public class CrimeActivity extends FragmentActivity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_crime);
-	}
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+        // calls getSupportFragmentManager to
+        // support pre-HoneyComb devices
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+        
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            manager.beginTransaction()
+                .add(R.id.fragmentContainer, fragment)
+                .commit();
+        }
+    }
 
 }
